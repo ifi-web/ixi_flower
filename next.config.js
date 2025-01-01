96d662e46b420cbc01266b17f042e5b7b65c9a3f
ifi-web <ixiflower.freakless2024@gmail.com>
2025-01-01 21:02:18 +0330

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export', // Required for GitHub Pages
-  basePath: '/ixi_flower', // Replace with your repository name
-  assetPrefix: '/ixi_flower'
+  basePath: '/ixi_flower', // Ensure this is your GitHub repo name
+  assetPrefix: '/ixi_flower' // Ensure this is your GitHub repo name
 }
