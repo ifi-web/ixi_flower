70afac1fc1de96e5064557f1bf6f0872032ab4dd
ifi-web <ixiflower.freakless2024@gmail.com>
2025-02-07 12:10:56 +0330

    first commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
   reactStrictMode: true,
   output: 'export',
   swcMinify: true,
   images: {
     unoptimized: true
-  },
-  basePath: isProd ? '/ixi_flower' : '',
-  assetPrefix: isProd ? '/ixi_flower/' : ''
+  }
 }
