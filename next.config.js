7dc08a21b58092910a87918c678cf0ce6aeab985
ifi-web <ixiflower.freakless2024@gmail.com>
2025-02-07 10:51:32 +0330

    first commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
   output: 'export',
-  swcMinify: true
+  swcMinify: true,
+  images: {
+    unoptimized: true
+  },
+
+  basePath: isProd ? '/ixi_flower' : '',
+  assetPrefix: isProd ? 'ixi_flower/' : ''
 }
--- out/_next/static/GH0BtMzHkX7Im2CMoY56K/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fposts","\u002Fwallpapers","\u002Fwallpapers\u002Fdark_wallpaper","\u002Fwallpapers\u002Fwallpaper","\u002Fworks","\u002Fworks\u002Fifi_web","\u002Fworks\u002Fixi_flower"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
